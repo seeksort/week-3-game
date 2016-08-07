4dcd56170c1706789525b8fa9a5097a5d37f4dcf
Kristin Faner <kfaner@gmail.com>
2016-08-07 17:52:02 -0500

    fixed regex, added reset, got game to work in console

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var states = ["Alabama", "Alaska",  "Arizona",  "Arkansas",  "California",  "Colorado",  "Connecticut",  "Delaware",  "Florida",  "Georgia",  "Hawaii",  "Idaho",  "Illinois",  "Indiana",  "Iowa",  "Kansas",  "Kentucky",  "Louisiana",  "Maine",  "Maryland",  "Massachusetts",  "Michigan",  "Minnesota",  "Mississippi",  "Missouri",  "Montana",  "Nebraska",  "Nevada",  "New Hampshire",  "New Jersey",  "New Mexico",  "New York",  "North Carolina",  "North Dakota",  "Ohio",  "Oklahoma",  "Oregon",  "Pennsylvania",  "Rhode Island",  "South Carolina",  "South Dakota",  "Tennessee",  "Texas",  "Utah",  "Vermont",  "Virginia",  "Washington",  "West Virginia",  "Wisconsin",  "Wyoming", "Washington DC", "Puerto Rico", "Guam", "Northern Marianas", "United States Virgin Islands", "American Samoa"]
+var states = ["Alabama", "Alaska",  "Arizona",  "Arkansas",  "California",  "Colorado",  "Connecticut",  "Delaware",  "Florida",  "Georgia",  "Hawaii",  "Idaho",  "Illinois",  "Indiana",  "Iowa",  "Kansas",  "Kentucky",  "Louisiana",  "Maine",  "Maryland",  "Massachusetts",  "Michigan",  "Minnesota",  "Mississippi",  "Missouri",  "Montana",  "Nebraska",  "Nevada",  "New Hampshire",  "New Jersey",  "New Mexico",  "New York",  "North Carolina",  "North Dakota",  "Ohio",  "Oklahoma",  "Oregon",  "Pennsylvania",  "Rhode Island",  "South Carolina",  "South Dakota",  "Tennessee",  "Texas",  "Utah",  "Vermont",  "Virginia",  "Washington",  "West Virginia",  "Wisconsin",  "Wyoming", "Washington DC", "Puerto Rico", "Guam", "Northern Marianas", "US Virgin Islands", "American Samoa"]
         
 var pacific = ["Alaska", "Hawaii"];
 var west = ["Arizona", "California", "Colorado", "Idaho",  "Montana", "Nevada", "New Mexico", "Oregon", "Utah", "Washington", "West Virginia", "Wyoming"];
@@ -6,12 +6,12 @@ var midwest = ["Illinois",  "Indiana",  "Iowa",  "Kansas", "Michigan",  "Minneso
 var northeast = ["Connecticut", "Maine",  "Massachusetts", "New Hampshire",  "New Jersey", "New York", "Pennsylvania", "Rhode Island", "Vermont"];
 var south = ["Alabama", "Arkansas", "Delaware", "Florida",  "Georgia", "Kentucky",  "Louisiana", "Maryland", "Mississippi",  "Missouri", "North Carolina", "Oklahoma", "South Carolina", "Tennessee",  "Texas", "Virginia"];
 var capitol = ["Washington DC"];
-var inhabitedTerritories = ["Puerto Rico", "Guam", "Northern Marianas", "United States Virgin Islands", "American Samoa"];
+var inhabitedTerritories = ["Puerto Rico", "Guam", "Northern Marianas", "US Virgin Islands", "American Samoa"];
 
 function chooseWord() {
 	var indexRand = Math.floor(Math.random() * 56);
 	// return states[indexRand].toUpperCase();
-	return states[54]; // left off here, testing guesses length
+	return states[54].toUpperCase(); // left off here, testing guesses length
 }
 // Display: a _ mer _  _ a
 // currentWordarray: ["A" "M" "E" "R" "I" "C" "A"]
@@ -36,7 +36,7 @@ function getCurrentLetter(event){;
 	}	
 }
 function isLetterInDisplay(currentLetter) {
-	if ((displayArray.indexOf(currentLetter) === -1) && (alreadyGuessed.indexOf(currentLetter) === -1) && (currentLetter.length == 1) && (currentLetter.search(/[A-Z]/) > -1)) { // TODO Regex not working!! only filters 1st letter!
+	if ((displayArray.indexOf(currentLetter) === -1) && (alreadyGuessed.indexOf(currentLetter) === -1) && (currentLetter.length == 1) && (currentLetter.search(/[a-zA-Z]/) > -1)) {
 		isLetterInCurrentWord(currentLetter);
 	} 
 }
@@ -59,18 +59,35 @@ function updateDisplayCurrentWord(currentLetter) {
 		} 
 	}
 	currentGuesses -= 1;
-	isGameActive()
-	console.log("Game Status: " + gameActive);
 	currentWordDisplay = displayArray.join("");
-	console.log("Current Guesses: " + currentGuesses);
-	console.log("New Display Array: " + displayArray);
-	console.log("Current Word Display: " + currentWordDisplay)
+	console.log(currentWord);
+	console.log(currentGuesses);
+	console.log("Current Word Display: " + currentWordDisplay);
+	isGameActive()
 }
 function isGameActive() {
-	if (currentGuesses == 0) {
+	if (currentWordDisplay == currentWord) {
 		gameActive = false;
-		// code for game over display
-	}
+		wins += 1;
+		statusMessage = "You win! Great job! Try another one!"
+		gameReset();
+	} else {
+		if (currentGuesses == 0) {
+			gameActive = false;
+			statusMessage = "You lose :( But try another one."
+			gameReset();
+		}
+	}	
+}
+function gameReset() {
+	currentWord = chooseWord();
+	currentWordArray = currentWord.split("");
+	displayArray = [];
+	currentWordDisplay = initialDisplayCurrentWord();
+	currentLetter = "";
+	currentGuesses = defaultGuesses;
+	alreadyGuessed = [];
+	gameActive = true;
 }
 
 
@@ -84,6 +101,7 @@ var currentGuesses = 20;
 var wins = 0;
 var alreadyGuessed = [];
 var gameActive = true;
+var statusMessage = "Play a new game!";
 
 console.log("Current Letter is: " + currentLetter);
 console.log(currentWordArray);
@@ -107,6 +125,20 @@ console.log(south);
 console.log(states.length);
 var total = pacific.concat(west,midwest,northeast,south,capitol,inhabitedTerritories)
 console.log("total: " + total.length);
+console.log("win?");
+console.log(currentWordDisplay == currentWord);
+console.log(currentWordDisplay);
+console.log(currentWord);
+}
+
+console.log("Game Status: " + gameActive);
+
+console.log("Current Guesses: " + currentGuesses);
+console.log("New Display Array: ")
+console.log(displayArray);
+console.log("Current Word Display: " + currentWordDisplay)
+console.log("Current Word Array: ")
+console.log(currentWordArray)
 */
 
 
