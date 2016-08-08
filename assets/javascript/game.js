b033f9ab13c84ab3115a341640cd218f066805b8
Kristin Faner <kfaner@gmail.com>
2016-08-07 22:11:49 -0500

    added HTML DOM manipulation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,30 +5,44 @@ var west = ["Arizona", "California", "Colorado", "Idaho",  "Montana", "Nevada", 
 var midwest = ["Illinois",  "Indiana",  "Iowa",  "Kansas", "Michigan",  "Minnesota", "Nebraska", "North Dakota",  "Ohio", "South Dakota", "Wisconsin"];
 var northeast = ["Connecticut", "Maine",  "Massachusetts", "New Hampshire",  "New Jersey", "New York", "Pennsylvania", "Rhode Island", "Vermont"];
 var south = ["Alabama", "Arkansas", "Delaware", "Florida",  "Georgia", "Kentucky",  "Louisiana", "Maryland", "Mississippi",  "Missouri", "North Carolina", "Oklahoma", "South Carolina", "Tennessee",  "Texas", "Virginia"];
-var capitol = ["Washington DC"];
+var capital = ["Washington DC"];
 var inhabitedTerritories = ["Puerto Rico", "Guam", "Northern Marianas", "US Virgin Islands", "American Samoa"];
+
+var currentWord = chooseWord();
+var currentWordArray = currentWord.split("");
+var correctlyGuessedArray = [];
+var displayArray = [];
+var currentWordDisplay = "";
+var currentLetter = "";
+var defaultGuesses = 20;
+var currentGuesses = 20;
+var wins = 0;
+var alreadyGuessed = [];
+var alreadyGuessedDisplay = alreadyGuessed.join(" ");
+var gameActive = true;
+var statusMessage = "Play a new game!";
 
 function chooseWord() {
 	var indexRand = Math.floor(Math.random() * 56);
 	// return states[indexRand].toUpperCase();
-	return states[54].toUpperCase(); // left off here, testing guesses length
+	return states[54].toUpperCase(); // debug: left off here, testing guesses length
 }
-// Display: a _ mer _  _ a
-// currentWordarray: ["A" "M" "E" "R" "I" "C" "A"]
-// DisplayArray: ["A" " _ " "E" "R" " _ " " _ " "A"]
-
 function initialDisplayCurrentWord() {
-	for (i = 0; i < currentWordArray.length; i++)
+	for (i = 0; i < currentWordArray.length; i++) {
 		if (currentWordArray[i] == " ") {
-			displayArray.push(" ");
+			displayArray.push("&nbsp");
+			correctlyGuessedArray.push(" ");
 		} else {
 			displayArray.push(" _ ");
+			correctlyGuessedArray.push(" _ ");
 		}
+	}
 	console.log("Display Array: " + displayArray);
 	currentWordDisplay = displayArray.join("");
+	isGameActive(updateHtml());
 	return currentWordDisplay;
 }
-function getCurrentLetter(event){;
+function getCurrentLetter(event){
 	if (gameActive) {
 		currentLetter = event.key.toUpperCase();
 		console.log(currentLetter);
@@ -46,16 +60,18 @@ function isLetterInCurrentWord(currentLetter) {
 	} else {
 		alreadyGuessed.push(currentLetter);
 		currentGuesses -= 1;
-		isGameActive()
-		console.log("Game Status: " + gameActive);
-		console.log("Current Guesses: " + currentGuesses);
-	}
+		isGameActive(updateHtml());		
+	}	
+	console.log("Game Status: " + gameActive);
+	console.log("Current Guesses: " + currentGuesses);
 }
 function updateDisplayCurrentWord(currentLetter) {
+	var wasHtmlUpdated = false;
 	var displayUpdate = "";
-	for (j = 0; j < currentWordArray.length; j++) {
-		if (currentLetter == currentWordArray[j]) {
-			displayArray.splice(j, 1, currentLetter);
+	for (k = 0; k < currentWordArray.length; k++) {
+		if (currentLetter == currentWordArray[k]) {
+			displayArray.splice(k, 1, currentLetter);
+			correctlyGuessedArray.splice(k, 1, currentLetter);
 		} 
 	}
 	currentGuesses -= 1;
@@ -63,45 +79,61 @@ function updateDisplayCurrentWord(currentLetter) {
 	console.log(currentWord);
 	console.log(currentGuesses);
 	console.log("Current Word Display: " + currentWordDisplay);
-	isGameActive()
+	isGameActive(updateHtml());	
 }
-function isGameActive() {
-	if (currentWordDisplay == currentWord) {
-		gameActive = false;
-		wins += 1;
-		statusMessage = "You win! Great job! Try another one!"
-		gameReset();
-	} else {
-		if (currentGuesses == 0) {
+function updateHtml() {
+	alreadyGuessedDisplay = alreadyGuessed.join(" ");
+	document.getElementById("statusMsgEl").innerHTML = statusMessage;
+	document.getElementById("wordDisplayEl").innerHTML = currentWordDisplay;
+	document.getElementById("winsEl").innerHTML = wins;
+	document.getElementById("guessesEl").innerHTML = currentGuesses;
+	document.getElementById("lettersGuessedDisplayEl").innerHTML = alreadyGuessedDisplay;
+	return true;
+}
+function isGameActive(htmlUpdated) {
+	var correctlyGuessedLetters = correctlyGuessedArray.join("");
+	console.log("Correctly Guessed:")
+	console.log(correctlyGuessedLetters);
+	if (htmlUpdated == true) {
+		if (correctlyGuessedLetters == currentWord) {
 			gameActive = false;
-			statusMessage = "You lose :( But try another one."
+			wins += 1;
+			statusMessage = "You win! Great job! Try another one!"
+			currentGuesses = defaultGuesses;
+			alreadyGuessed = [];
+			previousWordInfo()
 			gameReset();
+		} else {
+			if (currentGuesses == 0) {
+				gameActive = false;
+				statusMessage = "You lose :( But try another one."
+				currentGuesses = defaultGuesses;
+				alreadyGuessed = [];
+				previousWordInfo()
+				gameReset();
+			}
 		}
 	}	
+}
+function previousWordInfo() {
+	var newNode = document.createElement("a")
+	var currentWordLower = currentWord.toLowerCase();
+	var wikiUrl = "https://en.wikipedia.org/wiki/" + currentWordLower;
+	// newNode.innerHTML = currentWord;
+	document.getElementById("previousWordInfoEl").href = wikiUrl;
+	document.getElementById("previousWordInfoEl").innerHTML = currentWord;
+	console.log(wikiUrl);
 }
 function gameReset() {
 	currentWord = chooseWord();
 	currentWordArray = currentWord.split("");
+	correctlyGuessedArray = [];
 	displayArray = [];
 	currentWordDisplay = initialDisplayCurrentWord();
 	currentLetter = "";
-	currentGuesses = defaultGuesses;
-	alreadyGuessed = [];
+	alreadyGuessedDisplay = alreadyGuessed.join(" ");
 	gameActive = true;
 }
-
-
-var currentWord = chooseWord();
-var currentWordArray = currentWord.split("");
-var displayArray = [];
-var currentWordDisplay = "";
-var currentLetter = "";
-var defaultGuesses = 20;
-var currentGuesses = 20;
-var wins = 0;
-var alreadyGuessed = [];
-var gameActive = true;
-var statusMessage = "Play a new game!";
 
 console.log("Current Letter is: " + currentLetter);
 console.log(currentWordArray);
@@ -123,7 +155,7 @@ console.log(northeast);
 console.log(states.length);
 console.log(south);
 console.log(states.length);
-var total = pacific.concat(west,midwest,northeast,south,capitol,inhabitedTerritories)
+var total = pacific.concat(west,midwest,northeast,south,capital,inhabitedTerritories)
 console.log("total: " + total.length);
 console.log("win?");
 console.log(currentWordDisplay == currentWord);
